7837a9382df0aea1b3fc8654c2274a8712730a7c
JoakimKL <joakim.for.github@gmail.com>
2025-12-12 12:02:21 +0100

    Update balboa_balance.ino

--- src/balboa_balance/balboa_balance.ino
+++ src/balboa_balance/balboa_balance.ino
@@ -1,30 +1,3 @@
-// This example shows how to make a Balboa balance on its two
-// wheels and drive around while balancing.
-//
-// To run this demo, you will need to install the LSM6 library:
-//
-// https://github.com/pololu/lsm6-arduino
-//
-// To use this demo, place the robot on the ground with the
-// circuit board facing up, and then turn it on.  Be careful to
-// not move the robot for a few seconds after powering it on,
-// because that is when the gyro is calibrated.  During the gyro
-// calibration, the red LED is lit.  After the red LED turns off,
-// turn the robot so that it is standing up.  It will detect that
-// you have turned it and start balancing.
-//
-// Alternatively, you can press the A button while the robot is
-// lying down and it will try to use its motors to kick up into
-// the balancing position.
-//
-// This demo is tuned for the 50:1 high-power gearmotor with
-// carbon brushes, 45:21 plastic gears, and 80mm wheels; you will
-// need to adjust the parameters in Balance.h for your robot.
-//
-// After you have gotten the robot balance well, you can
-// uncomment some lines in loop() to make it drive around and
-// play a song.
-
 #include <Balboa32U4.h>
 #include <Wire.h>
 #include <LSM6.h>
